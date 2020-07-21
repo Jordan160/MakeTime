4b09d7b952f99aabe3f16dcca28fa81dea08d1a9
Jordan Vetter <jordan_vetter@optum.com>
2020-07-21 13:44:46 -0500

    Add DatePicker for better user experience

--- app/src/main/java/com/jvetter2/maketime/AddEditFragment.java
+++ app/src/main/java/com/jvetter2/maketime/AddEditFragment.java
@@ -1,6 +1,7 @@
 package com.jvetter2.maketime;
 
 
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -16,10 +17,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -68,11 +74,38 @@ public class AddEditFragment extends Fragment {
         final SQLiteDatabase eventDatabase = getActivity().openOrCreateDatabase("events", MODE_PRIVATE, null);
         nameET = view.findViewById(R.id.nameET);
         durationET = view.findViewById(R.id.durationET);
-        dateET = view.findViewById(R.id.dateET);
         save = view.findViewById(R.id.saveFloatingActionButton);
 
+        final Calendar myCalendar = Calendar.getInstance();
+        dateET = view.findViewById(R.id.dateET);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(myCalendar);
+            }
+        };
+
+
+
+        dateET.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
         saveNewEvent(eventDatabase);
     }
+
 
     private void saveNewEvent(final SQLiteDatabase eventDatabase) {
         save.setOnClickListener(new View.OnClickListener() {
@@ -100,5 +133,12 @@ public class AddEditFragment extends Fragment {
                 }
             //}
         });
+    }
+
+    private void updateLabel(Calendar myCalendar) {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dateET.setText(sdf.format(myCalendar.getTime()));
     }
 }
